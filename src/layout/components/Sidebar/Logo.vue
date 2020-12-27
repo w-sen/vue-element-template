13188f06c71bb9652b900351e5d4eface90fe698
wilson <411259243@qq.com>
2020-12-27 17:36:22 +0800

    页面修改

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -24,7 +24,7 @@ export default {
   },
   data() {
     return {
-      title: 'Vue Admin Template',
+      title: '嘉信盈合',
       logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
     }
   }
@@ -47,7 +47,7 @@ export default {
   height: 50px;
   line-height: 50px;
   background: #2b2f3a;
-  text-align: center;
+  padding-left: 20px;
   overflow: hidden;
 
   & .sidebar-logo-link {
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,30 +1,35 @@
 <template>
   <div class="dashboard-container">
     <div class="dashboard-text">name: {{ name }}</div>
+    <el-button type="primary">获取最新用户管理</el-button>
+    <el-button type="primary">获取最新标签管理</el-button>
+    <el-button type="primary">获取最新策略线索</el-button>
+    <el-button type="primary">获取最新销售线索-浏览行为</el-button>
+    <el-button type="primary">获取最新销售线索-浏览行为</el-button>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+  import { mapGetters } from 'vuex'
 
-export default {
-  name: 'Dashboard',
-  computed: {
-    ...mapGetters([
-      'name'
-    ])
+  export default {
+    name: 'Dashboard',
+    computed: {
+      ...mapGetters([
+        'name'
+      ])
+    }
   }
-}
 </script>
 
 <style lang="scss" scoped>
-.dashboard {
-  &-container {
-    margin: 30px;
+  .dashboard {
+    &-container {
+      margin: 30px;
+    }
+    &-text {
+      font-size: 30px;
+      line-height: 46px;
+    }
   }
-  &-text {
-    font-size: 30px;
-    line-height: 46px;
-  }
-}
 </style>
